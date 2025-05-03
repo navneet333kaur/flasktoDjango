7ff5a15044f0538f0708c8c2f11e268c87ba8991
navneet333kaur <193573879+navneet333kaur@users.noreply.github.com>
2025-05-03 12:34:29 +0530

    Update views.py

--- todoapp/views.py
+++ todoapp/views.py
@@ -3,3 +3,43 @@ from django.shortcuts import render
 # Defined the view for the home page
 def home_view(request):
     return render(request, 'home_backup.html')
+
+# Add these imports at the top if not already there
+from django.views.generic import ListView, DetailView
+from .models import Topic, Article  # Include your new models
+
+# Add your view classes
+class ProductivityHomeView(ListView):
+    model = Article
+    template_name = 'todoapp/productivity_guide/home.html'
+    context_object_name = 'featured_article'
+    
+    def get_queryset(self):
+        # Get most recent article as featured
+        return Article.objects.order_by('-created_at').first()
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['topics'] = Topic.objects.all()
+        return context
+
+class TopicDetailView(DetailView):
+    model = Topic
+    template_name = 'todoapp/productivity_guide/topic.html'
+    context_object_name = 'topic'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['topics'] = Topic.objects.all()
+        context['articles'] = Article.objects.filter(topic=self.object)
+        return context
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'todoapp/productivity_guide/article.html'
+    context_object_name = 'article'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['topics'] = Topic.objects.all()
+        return context
